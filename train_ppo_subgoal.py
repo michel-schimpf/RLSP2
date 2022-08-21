608ae8f8efa873a91f3d66542b9eeec10e9dfaa8
Michel Schimpf <michelschimpf@MacBook-Pro-von-Michel.local>
2022-08-21 19:17:03 +0200

    .

--- train_ppo_subgoal.py
+++ train_ppo_subgoal.py
@@ -11,30 +11,30 @@ from RL_PPA_monitor import RLPPAMonitor
 def train():
     models_dir = f"models/PPO"
     logdir = "logs"
-    TIMESTEPS = 8192
+    TIMESTEPS = 2048
     env = SubGoalEnv("pick-place-v2", rew_type="rew1")
     env_vec = SubprocVecEnv([lambda: env, lambda: env, lambda: env, lambda: env,
-                             lambda: env, lambda: env, lambda: env, lambda: env,
-                             lambda: env, lambda: env, lambda: env, lambda: env,
-                             lambda: env, lambda: env, lambda: env, lambda: env,
-                             lambda: env, lambda: env, lambda: env, lambda: env,
-                             lambda: env, lambda: env, lambda: env, lambda: env,
-                             lambda: env, lambda: env, lambda: env, lambda: env,
-                             lambda: env, lambda: env, lambda: env, lambda: env,
-                             lambda: env, lambda: env, lambda: env, lambda: env,
-                             lambda: env, lambda: env, lambda: env, lambda: env,
-                             lambda: env, lambda: env, lambda: env, lambda: env,
-                             lambda: env, lambda: env, lambda: env, lambda: env,
-                             lambda: env, lambda: env, lambda: env, lambda: env,
-                             lambda: env, lambda: env, lambda: env, lambda: env,
-                             lambda: env, lambda: env, lambda: env, lambda: env,
-                             lambda: env, lambda: env, lambda: env,  # lambda: env,
+                             # lambda: env, lambda: env, lambda: env, lambda: env,
+                             # lambda: env, lambda: env, lambda: env, lambda: env,
+                             # lambda: env, lambda: env, lambda: env, lambda: env,
+                             # lambda: env, lambda: env, lambda: env, lambda: env,
+                             # lambda: env, lambda: env, lambda: env, lambda: env,
+                             # lambda: env, lambda: env, lambda: env, lambda: env,
+                             # lambda: env, lambda: env, lambda: env, lambda: env,
+                             # lambda: env, lambda: env, lambda: env, lambda: env,
+                             # lambda: env, lambda: env, lambda: env, lambda: env,
+                             # lambda: env, lambda: env, lambda: env, lambda: env,
+                             # lambda: env, lambda: env, lambda: env, lambda: env,
+                             # lambda: env, lambda: env, lambda: env, lambda: env,
+                             # lambda: env, lambda: env, lambda: env, lambda: env,
+                             # lambda: env, lambda: env, lambda: env, lambda: env,
+                             # lambda: env, lambda: env, lambda: env,  # lambda: env,
                              ])
     env_vec = RLPPAMonitor(env_vec, "logs/PPO_0", )
     # right batch_size: https://github.com/llSourcell/Unity_ML_Agents/blob/master/docs/best-practices-ppo.md
     # TODO what are right paramters
     model = PPO('MlpPolicy', env_vec, verbose=1, tensorboard_log=logdir, n_steps=TIMESTEPS,
-                batch_size=16384, )
+                batch_size=4096, )
     # model = ALGO.load("models/PPO3/15360000.zip", env=env_vec,tensorboard_log=logdir)
     iters = 0
     while True:
@@ -42,7 +42,7 @@ def train():
         iters += 1
         model = model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False,
                             tb_log_name="PPO_0", )
-        model.save(f"{models_dir}/{TIMESTEPS * iters * 63}")
+        model.save(f"{models_dir}/{TIMESTEPS * iters * 4}")
 
 
 if __name__ == '__main__':
