248f2e87359c2c626f896af68cef9ee100a76bcc
Michel Schimpf <michelschimpf@MacBook-Pro-von-Michel.local>
2022-08-23 08:39:24 +0200

    .

---
+++ plots/plot_reward_comparison.py
@@ -0,0 +1,50 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn
+seaborn.set()
+
+plt.style.use("plots/subfigure.mplstyle")
+
+
+number_rows = 5000000
+values_combined = 300
+monitor = np.loadtxt("plots/normal.csv", delimiter=',', skiprows=2, max_rows=number_rows,usecols=(0,1,2,4))
+
+
+fig, ax = plt.subplots(constrained_layout=True)
+
+# time steps:
+x_time_steps_values = monitor[:, 1]
+size = len(x_time_steps_values) - (len(x_time_steps_values) % values_combined)
+x_time_steps_values = x_time_steps_values[:size]
+x_time_steps_values = x_time_steps_values.reshape((int(len(x_time_steps_values)/values_combined), values_combined))
+x_time_steps_values = np.sum(x_time_steps_values, axis=1)
+x_intervals = [0]
+for j in x_time_steps_values:
+    new_time_step = x_intervals[-1] + j
+    x_intervals.append(new_time_step)
+x_intervals = np.array(x_intervals)
+
+
+monitor = monitor[:size]
+# success rate:
+successes = monitor[:, 3]
+print(max(successes))
+successes = successes.reshape((int(size/values_combined), values_combined))
+average_success_rates = np.average(successes, axis=1)
+average_success_rates = np.insert(average_success_rates, 0, 0)
+
+
+# delete some values to make it more spiky
+for _ in range(2):
+    x_intervals = np.delete(x_intervals, np.arange(0, x_intervals.size, 2))
+    average_success_rates = np.delete(average_success_rates, np.arange(0, len(average_success_rates), 2))
+
+print(max(average_success_rates))
+ax.plot(x_intervals, average_success_rates, linewidth=1.3, label="Success Rate")
+ax.set_ylim([-0.05, 1])
+ax.set_xlabel("Steps")
+ax.set_ylabel("Success Rate")
+plt.legend(loc="lower right")
+plt.show()
--- train_ppo_subgoal.py
+++ train_ppo_subgoal.py
@@ -11,14 +11,14 @@ from RL_PPA_monitor import RLPPAMonitor
 def train():
     models_dir = f"models/PPO"
     logdir = "logs"
-    TIMESTEPS = 2048
+    TIMESTEPS = 256
     env = SubGoalEnv("pick-place-v2", rew_type="rew1")
     env_vec = SubprocVecEnv([lambda: env, lambda: env, lambda: env, lambda: env,
-                             # lambda: env, lambda: env, lambda: env, lambda: env,
-                             # lambda: env, lambda: env, lambda: env, lambda: env,
-                             # lambda: env, lambda: env, lambda: env, lambda: env,
-                             # lambda: env, lambda: env, lambda: env, lambda: env,
-                             # lambda: env, lambda: env, lambda: env, lambda: env,
+                             lambda: env, lambda: env, lambda: env, lambda: env,
+                             lambda: env, lambda: env, lambda: env, lambda: env,
+                             lambda: env, lambda: env, lambda: env, lambda: env,
+                             lambda: env, lambda: env, lambda: env, lambda: env,
+                             lambda: env, lambda: env, lambda: env, lambda: env,
                              # lambda: env, lambda: env, lambda: env, lambda: env,
                              # lambda: env, lambda: env, lambda: env, lambda: env,
                              # lambda: env, lambda: env, lambda: env, lambda: env,
@@ -42,7 +42,7 @@ def train():
         iters += 1
         model = model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False,
                             tb_log_name="PPO_0", )
-        model.save(f"{models_dir}/{TIMESTEPS * iters * 4}")
+        model.save(f"{models_dir}/{TIMESTEPS * iters * 24}")
 
 
 if __name__ == '__main__':
